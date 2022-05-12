1f704289b8a3046f2e46330dc2bb3c25a63c073e
samichaudhry <samichoudhry@outlook.com>
2022-05-12 17:25:42 +0500

    Round Button added & improved design for android

--- lib/custom_widgets.dart
+++ lib/custom_widgets.dart
@@ -1,16 +1,19 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:fyp_task/utils.dart';
 
 // Custom Button Widget
 Widget customButton(buttonText, onPressedFunc, ctx) {
-  return SizedBox(
+  return Container(
+    decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(95)),
+        color: Color(0xff009688)),
     height: responsiveHW(ctx, ht: 9),
     width: responsiveHW(ctx, wd: 35),
-    child: ElevatedButton(
+    child: TextButton(
       onPressed: onPressedFunc,
-      child: Text(buttonText, style: const TextStyle(fontSize: 20)),
+      child: Text(buttonText,
+          style: const TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600)),
     ),
   );
 }
